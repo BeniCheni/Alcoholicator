4468813633a14fae8075e48868a1b1f09a7659a6
Ben Chen <benjaminlchen@gmail.com>
2015-04-07 22:23:27 -0400

    Completed checkpoint-20-navigation

---
+++ Alcoholicator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcoholicator
+//
+//  Created by Beni Cheni on 4/7/15.
+//  Copyright (c) 2015 Princess of Darkness Factory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
